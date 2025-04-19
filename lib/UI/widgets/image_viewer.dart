4db57e6abfeaac92e0d2cae4fa64c45f41e51dd4
Shoxrux <yarashovshokhrukh@gmail.com>
2025-04-19 22:48:03 +0500

    imageviewer add new

---
+++ lib/UI/widgets/image_viewer.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ImageViewer extends StatelessWidget {
+  final String imagePath;
+  final double width;
+  final double height;
+  const ImageViewer({super.key, required this.imagePath, required this.width, required this.height});
+
+  void _showImageDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => Dialog(
+        backgroundColor: Colors.transparent,
+        child: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.black,
+              ),
+              padding: const EdgeInsets.all(10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(imagePath, fit: BoxFit.contain),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                icon: const Icon(Icons.close, color: Colors.white, size: 30),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _showImageDialog(context),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.network(
+          imagePath,
+          height: height,
+          width: width,
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) {
+            return Icon(Icons.error_outline);
+          },
+        ),
+      ),
+    );
+  }
+}
